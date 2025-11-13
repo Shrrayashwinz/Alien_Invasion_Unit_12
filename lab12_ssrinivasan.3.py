136c2dfdb7558f21c7c67f5e538932b101baa985
Shrrayashwinz <sshrrayash@gmail.com>
2025-11-12 21:15:31 -0500

    Created initial game loop and got the pygame setup!

--- lab12_ssrinivasan.3.py
+++ lab12_ssrinivasan.3.py
@@ -1,12 +1,22 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w, self.settings.screen_h)
+            )
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, 
+            (self.settings.screen_w, self.settings.screen_h)
+            )
 
         self.running = True
         self.clock = pygame.time.Clock()
@@ -18,7 +28,7 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
-
+            self.screen.blit(self.bg, (0,0))
             pygame.display.flip() 
             self.clock.tick(60)
 
