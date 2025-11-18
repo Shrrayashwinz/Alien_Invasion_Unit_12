da265aebe81623d9d48497a809e701cd4ad64caa
Shrrayashwinz <sshrrayash@gmail.com>
2025-11-18 17:07:52 -0500

    Okay. So now the aliens are dropping down by a certain point for each edge it hits. Whoo hoo, me.

--- settings.py
+++ settings.py
@@ -32,10 +32,11 @@ class Settings:
         self.ship_side = "right"
 
         self.alien_file = Path.cwd() / 'Assets' /  'images'   / 'enemy_4.png'
-        self.fleet_speed = 1.5
+        self.fleet_speed = 5
         self.alien_w = 40
         self.alien_h = 40
         self.fleet_direction = 1
+        self.fleet_drop_speed = 25
 
 
 
